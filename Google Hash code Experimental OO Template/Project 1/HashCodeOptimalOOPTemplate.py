e60ac0aa508b5e8d7d5f2e9f7a9f3307f1f54242
Exarchiasghost <exarchiasghost@gmail.com>
2017-01-31 22:15:33 +0100

    Saving the progress....

--- Google Hash code Experimental OO Template/Project 1/HashCodeOptimalOOPTemplate.py
+++ Google Hash code Experimental OO Template/Project 1/HashCodeOptimalOOPTemplate.py
@@ -70,8 +70,8 @@ def printFromArray(Arrayname, probablytheTVariable):
     for i in range(int(probablytheTVariable)):
         print(str(Arrayname[i]))
         
-#fills a two-dimensional array with the vale N, (the line from the input).
-#it needs to get called for every line from the main interation, (for every N)        
+#fills a two-dimensional array with the value N, (the line from the input).
+#it needs to get called for every line from the main iteration, (for every N)        
 def spliterFunction(strValueFromN):
     global spliterFunctionsArray
     spliterFunctionsArray.append(strValueFromN.split(' ')) 
